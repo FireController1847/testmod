159dde44b69795684da629e9e24c2079958a2fac
Unknown <usfirepilot123@gmail.com>
2017-10-09 23:22:09 -0600

    Rename TestMod for ResourcePacks (2)

--- src/main/java/com/firecontrol/testmodrp/Reference.java
+++ src/main/java/com/firecontrol/testmodrp/Reference.java
@@ -4,8 +4,8 @@ import net.minecraftforge.fml.common.versioning.VersionRange;
 
 public class Reference {
 
-	public static final String MOD_ID = "testmod";
-	public static final String NAME = "Test Mod";
+	public static final String MOD_ID = "testmodrp";
+	public static final String NAME = "Test Mod - ResourcePacks";
 	public static final String VERSION = "1.0.0";
 	public static final String MC_VERSIONS = "[1.12,1.13)";
 
